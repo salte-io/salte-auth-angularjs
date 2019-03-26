56bd66e660c072b08ba834f61de8fa4ac7ae97ed
Nick Woodward <nick@salte.io>
2019-03-26 09:44:24 -0500

    chore: updated dependencies (#222)
    - simplified webpack configs
    - added the ability to build branches beginning with "ci/*"

---
+++ webpack.common.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const webpack = require('webpack');
+const deindent = require('deindent');
+const { name, version, contributors, browserslist } = require('./package.json');
+
+module.exports = function({ minified, coverage, test }) {
+  return {
+    context: path.join(__dirname, 'src'),
+    entry: {
+      'salte-auth-angularjs': ['./salte-auth.module.js']
+    },
+    mode: 'none',
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: `[name]${minified ? '.min' : ''}.js`,
+      sourceMapFilename: '[file].map',
+      library: 'salte-auth-angularjs',
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    externals: test ? {} : {
+      'angular': 'angular',
+      '@salte-io/salte-auth': {
+        root: 'salte.auth',
+        commonjs: '@salte-io/salte-auth',
+        commonjs2: '@salte-io/salte-auth',
+        amd: '@salte-io/salte-auth'
+      }
+    },
+    devtool: test ? 'inline-source-map' : 'source-map',
+    module: {
+      rules: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules\/(?!(whatwg-url|@webcomponents|lit-html|lit-element|@polymer)\/).*/,
+        options: {
+          presets: [['@babel/preset-env', {
+            modules: false,
+            targets: {
+              browsers: browserslist
+            }
+          }]]
+        }
+      }].concat(coverage ? [{
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /tests|node_modules/,
+        use: {
+          loader: 'istanbul-instrumenter-loader',
+          options: { esModules: true }
+        }
+      }] : [])
+    },
+    optimization: {
+      minimize: minified ? true : false
+    },
+    plugins: [
+      new webpack.BannerPlugin({
+        banner: deindent(`
+          /**
+           * ${name} JavaScript Library v${version}
+           *
+           * @license MIT (https://github.com/salte-io/salte-auth-angularjs/blob/master/LICENSE)
+           *
+           * Made with ♥ by ${contributors.join(', ')}
+           */
+        `).trim(),
+        raw: true
+      })
+    ]
+  };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,11 @@
-const path = require('path');
-const webpack = require('webpack');
-const deindent = require('deindent');
-const packageJson = require('./package.json');
-const { argv: args } = require('yargs');
+const common = require('./webpack.common.config.js');
 
-const isProd = args.mode === 'production';
+module.exports = [
+  common({
+    minified: false
+  }),
 
-module.exports = {
-  context: path.join(__dirname, 'src'),
-  entry: {
-    'salte-auth-angularjs': ['./salte-auth.module.js']
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: isProd ? '[name].min.js' : '[name].js',
-    sourceMapFilename: '[file].map',
-    library: 'salte-auth-angularjs',
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  externals: {
-    'angular': 'angular',
-    '@salte-io/salte-auth': {
-      root: 'salte.auth',
-      commonjs: '@salte-io/salte-auth',
-      commonjs2: '@salte-io/salte-auth',
-      amd: '@salte-io/salte-auth'
-    }
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }, {
-      test: /\.html$/,
-      exclude: /node_modules/,
-      loader: 'html-loader'
-    }]
-  },
-  optimization: {
-    minimize: isProd ? true : false
-  },
-  plugins: [
-    new webpack.BannerPlugin({
-      banner: deindent(`
-        /**
-         * ${packageJson.name} JavaScript Library v${packageJson.version}
-         *
-         * @license MIT (https://github.com/salte-io/salte-auth/blob/master/LICENSE)
-         *
-         * Made with ♥ by ${packageJson.contributors.join(', ')}
-         */
-      `).trim(),
-      raw: true
-    })
-  ]
-};
+  common({
+    minified: true
+  })
+];
